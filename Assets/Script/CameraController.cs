68828043c997c5f14a65a1acfec11db2a980e610
SR21C <rysawad@gmail.com>
2021-11-22 12:35:00 +0900

    event用のアニメータークリップ

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
@@ -11,8 +12,9 @@ public class CameraController : MonoBehaviour
     [SerializeField] float lowAngleDistanceRatio = 0.5f;             // 低い時の距離の比率
     [SerializeField] float highAngleDistanceRatio = 3f;              // 高い時の距離の比率
     [SerializeField] float inputMappingCurve = 5f;
-    [SerializeField] float minCameraAngle = -45f;                     //X軸回転の下限
-    [SerializeField] float maxCameraAngle = 75f;　　　　　　　　　　  //Y軸回転の上限
+    [SerializeField] float minCameraAngle = -45f;                　　//X軸回転の下限
+    [SerializeField] float maxCameraAngle = 75f;　　　　　　　　　　 //Y軸回転の上限
+    [SerializeField] EnemyLife[] enemies = default;          　　　　//ロックオン対象の取得
     CinemachineVirtualCamera vCam = null;      　　                  //仮想カメラの参照
     Cinemachine3rdPersonFollow follow = null;　　　                  //仮想カメラの追跡対象の参照
     Vector2 cameraRotationInput = Vector2.zero;　　               　 //カメラのRotationの参照
@@ -24,45 +26,53 @@ public class CameraController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if(vCam != null)
+        if (vCam == null) return;
+        Transform target = vCam.Follow;
+        if (target != null)
         {
-            Transform target = vCam.Follow;
-            if (target != null)
+            //対象の回転を取得
+            Vector3 targetEulerAngles = target.rotation.eulerAngles;
+
+            float curve = Mathf.Max(1, inputMappingCurve);
+            Vector2 speed = new Vector2(
+                Mathf.Pow(Mathf.Abs(cameraRotationInput.x), curve) * Mathf.Sign(cameraRotationInput.x),
+                Mathf.Pow(Mathf.Abs(cameraRotationInput.y), curve) * Mathf.Sign(cameraRotationInput.y));
+            //targetEulerAngles.y += cameraRotationInput.x * rotationSpeed.y * Time.fixedDeltaTime;
+            //X軸、Y軸の回転を変える
+            targetEulerAngles.y += cameraRotationInput.x * rotationSpeed.y * Time.fixedDeltaTime;
+            targetEulerAngles.x += cameraRotationInput.y * rotationSpeed.x * Time.fixedDeltaTime;
+            //target.rotation.eulerAnglesは0～360の角度を返す。これを-180～180に変える
+            if (targetEulerAngles.x > 180f)
             {
-                //対象の回転を取得
-                Vector3 targetEulerAngles = target.rotation.eulerAngles;
-
-                float curve = Mathf.Max(1, inputMappingCurve);
-                Vector2 speed = new Vector2(
-                    Mathf.Pow(Mathf.Abs(cameraRotationInput.x), curve) * Mathf.Sign(cameraRotationInput.x),
-                    Mathf.Pow(Mathf.Abs(cameraRotationInput.y), curve) * Mathf.Sign(cameraRotationInput.y));
-                //targetEulerAngles.y += cameraRotationInput.x * rotationSpeed.y * Time.fixedDeltaTime;
-                //X軸、Y軸の回転を変える
-                targetEulerAngles.y += cameraRotationInput.x * rotationSpeed.y * Time.fixedDeltaTime;
-                targetEulerAngles.x += cameraRotationInput.y * rotationSpeed.x * Time.fixedDeltaTime;
-                //target.rotation.eulerAnglesは0～360の角度を返す。これを-180～180に変える
-                if (targetEulerAngles.x > 180f)
-                {
-                    targetEulerAngles.x -= 360f;
-                }
-                // この状態で値を制限する。
-                // 「Clamp」は一つ目の引数を2つ目と3つ目の引数の間に制限するメソッド
-                targetEulerAngles.x = Mathf.Clamp(targetEulerAngles.x, minCameraAngle, maxCameraAngle);
-
-                // オイラー角度をクオータニオンに変換して追跡ターゲットの回転を変える
-                target.transform.rotation = Quaternion.Euler(targetEulerAngles);
-                //カメラの位置調整（カメラが低い位置に下がるほどキャラクターに近づいて、高くなるほど遠ざかる。）
-                if(follow)
-                {
-                    float anglePhase = (targetEulerAngles.x - minCameraAngle) / (maxCameraAngle - minCameraAngle);
-                    float lowCameraDistance = cameraDistance * lowAngleDistanceRatio;
-                    float highCameraDistance = cameraDistance * highAngleDistanceRatio;
-                    follow.CameraDistance = lowCameraDistance + (highCameraDistance - lowCameraDistance) * anglePhase;
-
-                }
+                targetEulerAngles.x -= 360f;
             }
-        }
+            // この状態で値を制限する。
+            // 「Clamp」は一つ目の引数を2つ目と3つ目の引数の間に制限するメソッド
+            targetEulerAngles.x = Mathf.Clamp(targetEulerAngles.x, minCameraAngle, maxCameraAngle);
+
+            // オイラー角度をクオータニオンに変換して追跡ターゲットの回転を変える
+            target.transform.rotation = Quaternion.Euler(targetEulerAngles);
+            // カメラの位置調整（カメラが低い位置に下がるほどキャラクターに近づいて、高くなるほど遠ざかる。）
+            if(follow)
+            {
+                float anglePhase = (targetEulerAngles.x - minCameraAngle) / (maxCameraAngle - minCameraAngle);
+                float lowCameraDistance = cameraDistance * lowAngleDistanceRatio;
+                float highCameraDistance = cameraDistance * highAngleDistanceRatio;
+                follow.CameraDistance = lowCameraDistance + (highCameraDistance - lowCameraDistance) * anglePhase;
+
+            }
+            }
+        
     }
+    //void LockOn(float distance,bool state)
+    //{
+    //    if (!state) return;
+    //    var nearenemy = enemies.OrderBy(x => distance > Vector3.Distance(x.transform.position, this.gameObject.transform.position)).ToList();
+    //    if (nearenemy == null)
+    //    {
+    //        var player = GameObject.Find("Paladin");
+    //    }
+    //}
     void Look(Vector2 input)
     {
         cameraRotationInput = input;
@@ -71,4 +81,9 @@ public class CameraController : MonoBehaviour
     {
         Look(inputValue.Get<Vector2>());
     }
+    //void OnLockOn(InputValue inputValue)
+    //{
+    //    Debug.Log("ON");
+    //    LockOn(5f,inputValue.isPressed);
+    //}
 }
